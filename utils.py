8cb45822f5fee4da47442d96bd3a1fa24fb2674e
Lekuru <contact@lekuru.xyz>
2025-05-27 18:27:33 +0200

    Handle more edge cases in ip resolver

--- utils.py
+++ utils.py
@@ -5,17 +5,17 @@ def resolve_ip_address(websocket: WebSocketServerProtocol) -> str:
     headers = websocket.request_headers
 
     if 'CF-Connecting-IP' in headers:
-        return headers['CF-Connecting-IP']
+        return headers['CF-Connecting-IP'].strip()
 
     if 'X-Real-IP' in headers:
         return headers['X-Real-IP'].strip()
     
     if 'X-Forwarded-For' in headers:
-        return headers['X-Forwarded-For'].split(',')[0]
+        return headers['X-Forwarded-For'].split(',')[0].strip()
 
     address = websocket.remote_address
 
-    if address is None:
+    if not address:
         return ''
-    
-    return address[0]
+
+    return address[0] or ''
